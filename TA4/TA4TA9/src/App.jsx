c259c18c0e8735773422b567ace9db5035d540d4
Diego Vázquez <dvazquezlagreca@gmail.com>
2024-09-18 18:53:48 -0300

    TA9

--- TA4/TA4TA9/src/App.jsx
+++ TA4/TA4TA9/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,9 +6,14 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  // Utiliza useEffect para cambiar el título cada vez que el contador cambie
+  useEffect(() => {
+    document.title = `Contador: ${count}`
+  }, [count])  // El hook se ejecuta cada vez que 'count' cambia
+
   return (
     <>
-      <h1>TA4</h1>
+      <h1>TA4 + TA9</h1>
       <h2>Count is {count}</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
