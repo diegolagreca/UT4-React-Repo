f6bc0e4f5cf3f735c4c260b3fa709ae5e39a9ecf
Diego Vázquez <dvazquezlagreca@gmail.com>
2025-07-06 15:30:14 -0300

    Fix ProtectedRoute to render children

--- TA18/src/components/ProtectedRoute.jsx
+++ TA18/src/components/ProtectedRoute.jsx
@@ -1,14 +1,18 @@
 function ProtectedRoute({ isProtected, children }) {
     if (isProtected) {
-        return <div>
-        <h2>🔓 Unlocked 🔓</h2>
-        <p>You are adopted</p>
-    </div>;
+        return (
+            <div>
+                <h2>🔓 Unlocked 🔓</h2>
+                {children}
+            </div>
+        );
     }
-    return <div>
-        <h2>🔒 Locked 🔒</h2>
-        <p>🛑 Nothig to see here 🛑</p>
-    </div>;
+    return (
+        <div>
+            <h2>🔒 Locked 🔒</h2>
+            <p>🛑 Nothing to see here 🛑</p>
+        </div>
+    );
 }
 
 export default ProtectedRoute;
